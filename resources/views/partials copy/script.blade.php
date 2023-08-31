c12c9f54e298e547309c810b30f95d63f42c1df5
samad-dev <abdulsamadabdulqadir6@gmail.com>
2023-08-31 12:59:20 +0500

    Files

---
+++ resources/views/partials copy/script.blade.php
@@ -0,0 +1,56 @@
+<script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/metismenujs/metismenujs.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/eva-icons/eva.min.js') }}"></script>
+
+        <!-- apexcharts -->
+        <script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+        <!-- Vector map-->
+        <script src="{{ asset('assets/libs/jsvectormap/js/jsvectormap.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/jsvectormap/maps/world-merc.js') }}"></script>
+        
+        <script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script>
+
+        <script src="{{ asset('assets/js/app.js') }}"></script>
+          <!-- gridjs js -->
+          <script src="{{ asset('assets/libs/gridjs/gridjs.umd.js') }}"></script>
+
+          <script src="{{ asset('assets/js/pages/gridjs.init.js') }}"></script>
+          <script src="{{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+          <!-- Buttons examples -->
+          <script src="{{ asset('assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/jszip/jszip.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
+          <script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+  
+          <!-- Responsive examples -->
+          <script src="{{ asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+          <script src="{{ asset('assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+          <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.colVis.min.js"></script>
+          <!-- Datatable init js -->
+          <!-- choices js -->
+        <script src="{{ asset('assets/libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>
+          {{-- <script src="assets/libs/gridjs/gridjs.umd.js"></script> --}}
+  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script> --}}
+  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js"></script> --}}
+<!-- choices js -->
+<!-- Plugins js -->
+<script src="{{ asset('assets/libs/dropzone/min/dropzone.min.js') }}"></script>
+<script src="{{ asset('assets/libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>
+{{-- <script src="{{ asset('assets/js/pages/form-advanced.init.js') }}"></script> --}}
+ <!-- Sweet Alerts js -->
+ <script src="{{ asset('assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+
+ <!-- Sweet alert init js-->
+ <script src="{{ asset('assets/js/pages/sweet-alerts.init.js') }}"></script>
+ <script src="{{ asset('assets/js/pages/form-validation.init.js') }}"></script>
+ <script src="{{ asset('assets/libs/flatpickr/flatpickr.min.js') }}"></script>
+
+          
+{{-- <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script> --}}
---
+++ resources/views/partials copy/style.blade.php
@@ -0,0 +1,23 @@
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+        <!-- plugin css -->
+        <link href="{{ asset('assets/libs/jsvectormap/css/jsvectormap.min.css') }}" rel="stylesheet" type="text/css" />
+
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+        <!-- gridjs css -->
+        <link href="{{ asset('assets/libs/dropzone/min/dropzone.min.css') }}" rel="stylesheet" type="text/css" />
+
+        <link rel="stylesheet" href="{{ asset('assets/libs/gridjs/theme/mermaid.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css') }}">
+      
+        <link href="{{ asset('assets/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- choices css -->
+        
+        <link href="{{ asset('assets/libs/choices.js/public/assets/styles/choices.min.css') }}" rel="stylesheet" type="text/css" />
+       <link rel="stylesheet" href="{{ asset('assets/libs/flatpickr/flatpickr.min.css') }}">
