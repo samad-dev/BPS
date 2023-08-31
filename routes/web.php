0ccf32e5fbbf501636b8359da5f1173482b7400e
samad-dev <abdulsamadabdulqadir6@gmail.com>
2023-08-31 13:00:25 +0500

    web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\VendorTypeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
+
 Route::get('/index', function () {
     return view('dashboard/index');
 });
@@ -28,6 +32,41 @@ Route::get('/index', function () {
 // Route::get('/vendor-types', function () {
 //     return view('types/index');
 // });
+Route::get('/vendor-types', function () {
+    $types = DB::table('types')->select('company_id','title','parent_id','active')->get();
+    return view('types/index', compact('types'));
+});
+Route::get('/status', function () {
+    return view('status/index');
+});
+
+Route::get('/priority', function () {
+    return view('priority/index');
+});
+Route::get('/impact', function () {
+    return view('impact/index');
+});
+Route::get('/groups', function () {
+    return view('groups/index');
+});
+Route::get('/domain', function () {
+    return view('domain/index');
+});
+Route::get('/bu', function () {
+    return view('bu/index');
+});
+Route::get('/vendor', function () {
+    return view('vendor/index');
+});
+Route::get('/Subscription', function () {
+    return view('Subscription/index');
+});
+Route::get('/Tickets', function () {
+    return view('Tickets/index');
+});
+Route::get('/Tickets/{id}', function () {
+    return view('Tickets/new');
+});
 Route::get('/Permission', function () {
     return view('um/permission');
 });
@@ -36,4 +75,10 @@ Route::get('/role', function () {
 });
 Route::get('/user', function () {
     return view('um/user');
+});
+Route::get('/designation', function () {
+    return view('um/designation');
+});
+Route::get('/Company', function () {
+    return view('company/index');
 });
